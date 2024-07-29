cc2a368349a022974f385a7d9f64853472e407e8
GnuWorm32 <stevanglavaskii@gmail.com>
2024-07-29 19:01:04 +0200

    Javascript jQuery

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.hamburger-menu').click(function(event){
+        event.stopPropagation();
+        if ($('main').hasClass('active')) {
+            $('main').removeClass('active');
+        } else {
+            $('main').addClass('active');
+        }
+    });
+});
